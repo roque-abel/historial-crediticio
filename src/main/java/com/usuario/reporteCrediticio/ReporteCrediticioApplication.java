722fa8365a4a55202af65f1c13b5b188bc79106a
roque-abel <roqueabel0409@gmail.com>
2025-10-07 01:03:05 -0600

    integrando dependencias

---
+++ src/main/java/com/usuario/reporteCrediticio/ReporteCrediticioApplication.java
@@ -0,0 +1,13 @@
+package com.usuario.reporteCrediticio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteCrediticioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteCrediticioApplication.class, args);
+	}
+
+}
